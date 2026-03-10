290429263505e32722ee5366a0ac321d40840391
abderrahmane-mansir <mansir.0.abderrahmane@gmail.com>
2026-03-10 21:21:37 +0000

    ex1

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -4,20 +4,26 @@ import sys
 def main() -> None:
     print("=== Player Score Analytics ===")
     if len(sys.argv) == 1:
-        raise ValueError("No scores provided!")
-    scores = []
-    total = 0
-    for i in range(1, len(sys.argv)):
-        try:
-            score = int(sys.argv[i])
-            total += score
-            scores.append(score)
-        except ValueError:
-            print(f"Invalid score '{sys.argv[i]}', skipping.")
-    print("Scores received: ", scores)
-    print("Total Players: ", len(scores))
-    print("Total Score: ", total)
-    print("Average Score: ", total / len(scores) if scores else 0)
+        print("No scores provided."
+              " Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+    else:
+        scores = []
+        total = 0
+        for i in range(1, len(sys.argv)):
+            try:
+                score = int(sys.argv[i])
+                total += score
+                scores.append(score)
+            except ValueError:
+                print(f"Invalid score '{sys.argv[i]}', skipping.")
+        print("Scores received: ", scores)
+        print("Total Players: ", len(scores))
+        print("Total Score: ", total)
+        print("Average Score: ", total / len(scores) if scores else 0)
+        print("High Score: ", max(scores) if scores else 0)
+        print("Low Score: ", min(scores) if scores else 0)
+        print("Score Range: ", max(scores) - min(scores) if scores else 0,
+              end="\n\n")
 
 
 if __name__ == "__main__":
